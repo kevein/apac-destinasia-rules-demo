872a183a41c55026368319b03214e12fcc080994
Kevein Liu <liuyang3240@gmail.com>
2017-03-08 16:34:41 +0800

    Add scaling.sh

---
+++ scaling.sh
@@ -0,0 +1,16 @@
+#!/bin/sh 
+
+for pods in {2..10}
+do
+	echo; echo "Scaling up to $pods pods..."; echo;
+	oc scale dc/destinasia-rules-demo --replicas=$pods
+	sleep 5
+done
+
+for pods in {9..1}
+do
+	echo; echo "Scaling down to $pods pods..."; echo;
+	oc scale dc/destinasia-rules-demo --replicas=$pods
+	sleep 5
+done
+
